c76464ff2cf52e40946f70aeb7db0ba1ade1f885
João Nogueira <joaoporto2001@gmail.com>
2023-11-03 16:16:09 +0000

    initial implementation of the algorithm

---
+++ src/main/java/com/isep/acme/model/ReviewRecommendationsAlgo2.java
@@ -0,0 +1,64 @@
+package com.isep.acme.model;
+
+import com.isep.acme.services.iRepositories.ReviewRepository;
+import com.isep.acme.services.iRepositories.UserRepository;
+import com.isep.acme.services.iServices.ReviewRecommendationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Component
+public class ReviewRecommendationsAlgo2 implements ReviewRecommendationService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Override
+    public List<Review> getRecommendations(List<Review> allReviews) {
+        List<Review> recommendedReviews = new ArrayList<>();
+        Long currentUserId = 0L;
+        AtomicInteger currentUserTotalVotes = new AtomicInteger();
+        Map<Long, Integer> votesPerUser = new TreeMap<>();
+        
+        allReviews.forEach(review -> {
+            currentUserTotalVotes.addAndGet(GetUsersWithSameVotes(review.getUpVote(), currentUserId, votesPerUser));
+            currentUserTotalVotes.addAndGet(GetUsersWithSameVotes(review.getDownVote(), currentUserId, votesPerUser));
+        });
+
+        for (var entry : votesPerUser.entrySet()) {
+            if (((float) entry.getValue()) / currentUserTotalVotes.get() >= 0.5) {
+                var user = userRepository.findById(entry.getKey());
+                if (user.isPresent()) {
+                    var reviews = reviewRepository.findByUserId(user.get());
+                    recommendedReviews.addAll(reviews);
+                }
+            }
+        }
+
+        return recommendedReviews;
+    }
+
+    private int GetUsersWithSameVotes(List<Vote> votes, Long currentUserId, Map<Long, Integer> votesPerUser) {
+        if (votes.stream().anyMatch(vote -> vote.getUserID().equals(currentUserId))) {
+            votes.forEach(vote -> {
+                if (votesPerUser.containsKey(vote.getUserID())) {
+                    votesPerUser.replace(vote.getUserID(), votesPerUser.get(vote.getUserID()) + 1);
+                } else {
+                    votesPerUser.put(vote.getUserID(), 1);
+                }
+            });
+
+            return 1;
+        }
+
+        return 0;
+    }
+}
